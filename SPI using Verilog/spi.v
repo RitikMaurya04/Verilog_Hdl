7c11b5e025dd62652391ed2b2177ee1c7158264f
RitikMaurya04 <mauryaritik04@gmail.com>
2026-03-16 13:00:53 +0530

    Refactor SPI module and add spi_master module

--- SPI using Verilog/spi.v
+++ SPI using Verilog/spi.v
@@ -1,80 +1,115 @@
 //SPI
 
-`timescale 1ns / 1ps
 
-module spi
+
+`timescale 1ns/1ps
+
+module spi_master
+#(
+    parameter CPOL = 0,
+    parameter CPHA = 0
+)
 (
-    input clk, start,
+    input clk,
+    input start,
     input [11:0] din,
-    output reg cs, mosi, done,
-    output sclk
+
+    output reg mosi,
+    output reg cs,
+    output reg done,
+    output reg sclk
 );
 
-integer count = 0;
-reg sclk = 0;
+//////////////////////////////////////
+// clock divider
+//////////////////////////////////////
+
+reg [3:0] clkdiv = 0;
 
 always @(posedge clk)
 begin
-    if (count < 10)
-        count <= count + 1;
-    else
-        begin
-            count <= 0;
-            sclk <= ~sclk;
-        end
+    clkdiv <= clkdiv + 1;
 end
 
-////////////////////////////////////
-parameter idle = 0, start_tx = 1, send = 2, end_tx = 3;
-reg [1:0] state = idle;
-reg [11:0] temp;
-integer bitcount = 0;
+wire spi_clk_en = (clkdiv == 0);
 
-always @(posedge sclk)
+//////////////////////////////////////
+// FSM
+//////////////////////////////////////
+
+parameter IDLE = 0,
+          LOAD = 1,
+          TRANSFER = 2,
+          DONE = 3;
+
+reg [1:0] state = IDLE;
+
+reg [11:0] shift_reg;
+reg [3:0] bitcount;
+
+//////////////////////////////////////
+// SCLK generation
+//////////////////////////////////////
+
+always @(posedge clk)
+begin
+    if(state == IDLE)
+        sclk <= CPOL;
+    else if(spi_clk_en)
+        sclk <= ~sclk;
+end
+
+//////////////////////////////////////
+// SPI FSM
+//////////////////////////////////////
+
+always @(posedge clk)
 begin
     case(state)
-        idle: begin
-            mosi <= 1'b0;
-            cs <= 1'b1;
-            done <= 1'b0;
 
-            if(start)
-                state <= start_tx;
-            else
-                state <= idle;
-        end
+    IDLE:
+    begin
+        cs <= 1;
+        done <= 0;
 
-        start_tx: begin
-            cs <= 1'b0;
-            temp <= din;
-            state <= send;
-        end
+        if(start)
+            state <= LOAD;
+    end
 
-        send: begin
-            if(bitcount <= 11)
+    LOAD:
+    begin
+        cs <= 0;
+        shift_reg <= din;
+        bitcount <= 0;
+        state <= TRANSFER;
+    end
+
+    TRANSFER:
+    begin
+        if(spi_clk_en)
+        begin
+
+            if((CPHA==0 && sclk==~CPOL) || (CPHA==1 && sclk==CPOL))
             begin
+                mosi <= shift_reg[11];
+                shift_reg <= shift_reg << 1;
                 bitcount <= bitcount + 1;
-                mosi <= temp[bitcount];
-                state <= send;
             end
-            else
-            begin
-                bitcount <= 0;
-                state <= end_tx;
-                mosi <= 1'b0;
-            end
-        end
 
-        end_tx: begin
-            cs <= 1'b1;
-            state <= idle;
-            done <= 1'b1;
-        end
+            if(bitcount == 12)
+                state <= DONE;
 
-        default: state <= idle;
+        end
+    end
+
+    DONE:
+    begin
+        cs <= 1;
+        done <= 1;
+        state <= IDLE;
+    end
+
     endcase
 end
-
-assign sclk = sclkt;
 
 endmodule
