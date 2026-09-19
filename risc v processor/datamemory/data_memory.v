ee43cc620097a84a09959c16381cf875d7da6697
RitikMaurya04 <mauryaritik04@gmail.com>
2026-09-19 22:05:32 +0530

    Add data_memory module for RISC-V processor

---
+++ risc v processor/datamemory/data_memory.v
@@ -0,0 +1,23 @@
+module data_memory (
+    input        clk,
+    input [31:0] waddr,
+    input [31:0] raddr,
+    input [31:0] wdata,
+    input        wen,
+    input        ren,
+    output wire [31:0] rdata
+);
+
+    // 256 words × 32 bits = 8,192 bits = 1 KB
+    reg [31:0] data [0:255];
+
+    // Synchronous write
+    always @(posedge clk) begin
+        if (wen)
+            data[waddr[9:2]] <= wdata;
+    end
+
+    // Asynchronous read
+    assign rdata = ren ? data[raddr[9:2]] : 32'b0;
+
+endmodule
